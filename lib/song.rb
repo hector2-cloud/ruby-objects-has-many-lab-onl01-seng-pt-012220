9d334350b4e4c2786c6b46a7740bb632413aad47
awesome-writer-4373 <hectoraperez1999@gmail.com>
2020-02-24 17:21:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,31 @@
 class Songs 
+  attr_accessor :title, :artist 
+
+  @@all = []
+
+  def initialize(title)
+    @title = title 
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def name 
+    @title 
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil 
+    end
+  end
+
 end 
